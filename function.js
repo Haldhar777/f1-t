f02f5b5d41fc4cfd6a1ebc28c395a987bd42ae5d
Haldhar777 <haldharpatel.hp777@gmail.com>
2022-06-23 23:07:09 +0530

    separated js from html

--- function.js
+++ function.js
@@ -1,7 +1,34 @@
-const newin = document.getElementsByClassName("newInput")[0].value;
 const msg = document.getElementById("msg");
 
+const newTitle = document.getElementById("title");
+
+const btntext = document.getElementById("btntext");
+
+const toberemoved = document.getElementById("toberemoved");
+
+const succes_msg = document.getElementById("succes_msg");
+
+let value1 = "";
+let value2 = "";
+
+const myFunction1 = (val) => {
+  value1 = val;
+};
+const myFunction2 = (val) => {
+  value2 = val;
+};
+
 const checkPass = () => {
-  console.log(newin);
-  msg.innerHTML = newin;
+  if (value1 != value2) {
+    msg.innerHTML = "Passwords do not match";
+  } else {
+    newTitle.textContent = "Password Changed";
+    succes_msg.innerHTML =
+      "Your password has been updated! Now get back " +
+      "</br>" +
+      "in the driver seat, racers are waiting for you!";
+
+    btntext.textContent = "JOIN A RACE";
+    toberemoved.innerHTML = "";
+  }
 };
